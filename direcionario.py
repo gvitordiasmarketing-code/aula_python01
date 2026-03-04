b57ea83578c12647e8dca81f2e2ee98d39335547
Gabriel Vitor Dias Rocha <1051633@senacminas.edu.br>
2026-03-04 09:09:34 -0300

    eventos

---
+++ direcionario.py
@@ -0,0 +1,10 @@
+meu_direcionario = {
+    "Nome" : "Gabriel" ,
+    "sobrenome" : "Gabriel Vitor" ,
+    "idade" : "21" ,
+    "cor preferida" : "verde" ,
+    "animal de estimação" : "gato e cachorro" ,
+    "Escolaridade" : "Ensino médio completo"
+}
+
+print(meu_direcionario)
