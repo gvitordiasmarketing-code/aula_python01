7b1c2596ec6245b794d0aa46e2318d8b96ce604d
Gabriel Vitor Dias Rocha <1051633@senacminas.edu.br>
2026-03-06 09:40:33 -0300

    cases

---
+++ cases.py
@@ -0,0 +1,19 @@
+escolha = int(input("Informe um numero de 1 a 7 para escolher o dia na semana:"))
+
+
+match escolha:
+    case 1:
+        print("Segunda")
+    case 2:
+        print("Terça")
+    case 3:
+        print("Quarta")
+    case 4:
+        print("Quinta")
+    case 5:
+        print("Sexta")
+    case 6:
+        print("Sábado")
+    case 7:
+        print("Domingo")
+
